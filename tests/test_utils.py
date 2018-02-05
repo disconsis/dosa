efb0c70e1adca38351e8a9f937b0cda1926a6089
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2018-02-05 16:39:32 +0530

    expand first test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,13 @@
 import unittest
 import utils
+import netaddr
 
 
 class MACTest(unittest.TestCase):
 
     def test_mac_has_broadcast_addr(self):
         self.assertEqual(str(utils.MAC.broadcast), 'ff:ff:ff:ff:ff:ff')
+        self.assertEqual(utils.MAC.broadcast, netaddr.EUI('ff:ff:ff:ff:ff:ff'))
 
 
 if __name__ == '__main__':
