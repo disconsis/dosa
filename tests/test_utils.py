242c01121bd311737726668f9f70f1732ac750f1
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2018-02-05 22:41:26 +0530

    changed to pytest testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,17 @@
-from nose.tools import assert_equal
 import utils
 import netaddr
 
 
 def test_mac_has_broadcast_addr():
-    assert_equal(str(utils.MAC.broadcast), 'ff:ff:ff:ff:ff:ff')
-    assert_equal(utils.MAC.broadcast, netaddr.EUI('ff:ff:ff:ff:ff:ff'))
+    assert str(utils.MAC.broadcast) == 'ff:ff:ff:ff:ff:ff'
+    assert utils.MAC.broadcast == netaddr.EUI('ff:ff:ff:ff:ff:ff')
 
 
 def test_mac_takes_argument_and_creates_eui_object():
-    assert_equal(
-        utils.MAC('11:22:33:44:55:66'), netaddr.EUI('11:22:33:44:55:66')
-    )
-    assert_equal(
-        utils.MAC('12:34:56:78:9a:bc'), netaddr.EUI('12:34:56:78:9a:bc')
-    )
+    assert utils.MAC('11:22:33:44:55:66') == netaddr.EUI('11:22:33:44:55:66')
+    assert utils.MAC('12:34:56:78:9a:bc') == netaddr.EUI('12:34:56:78:9a:bc')
 
 
 def test_mac_has_valid_str_representation():
-    assert_equal(str(utils.MAC('11:22:33:44:55:66')), '11:22:33:44:55:66')
-    assert_equal(str(utils.MAC('12:34:56:78:9a:bc')), '12:34:56:78:9a:bc')
+    assert str(utils.MAC('11:22:33:44:55:66')) == '11:22:33:44:55:66'
+    assert str(utils.MAC('12:34:56:78:9a:bc')) == '12:34:56:78:9a:bc'
