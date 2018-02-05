9987a342208528605687efd7be91458633970611
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2018-02-05 22:46:10 +0530

    add tests for utils.Host

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,3 +15,16 @@ def test_mac_takes_argument_and_creates_eui_object():
 def test_mac_has_valid_str_representation():
     assert str(utils.MAC('11:22:33:44:55:66')) == '11:22:33:44:55:66'
     assert str(utils.MAC('12:34:56:78:9a:bc')) == '12:34:56:78:9a:bc'
+
+
+def test_host_takes_argument_ip_and_sets_ip_attr_as_netaddr_ipaddress_object():
+    assert utils.Host('1.2.3.4').ip == netaddr.IPAddress('1.2.3.4')
+    assert utils.Host('23.22.11.58').ip == netaddr.IPAddress('23.22.11.58')
+
+
+def test_host_takes_optional_mac_argument_and_sets_mac_attr_as_eui_object():
+    assert utils.Host('1.2.3.4', mac='12:34:56:78:9a:bc').mac \
+        == netaddr.EUI('12:34:56:78:9a:bc')
+    assert utils.Host('1.1.1.1', mac='ab:cd:ef:12:34:56').mac \
+        == netaddr.EUI('ab:cd:ef:12:34:56')
+    assert utils.Host('1.1.1.1').mac is None
