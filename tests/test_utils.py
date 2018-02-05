81d2ccd62a46ef7489e7e6262fbff876a5b5a38b
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2018-02-05 16:59:45 +0530

    add more tests for utils.MAC

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,3 +6,17 @@ import netaddr
 def test_mac_has_broadcast_addr():
     assert_equal(str(utils.MAC.broadcast), 'ff:ff:ff:ff:ff:ff')
     assert_equal(utils.MAC.broadcast, netaddr.EUI('ff:ff:ff:ff:ff:ff'))
+
+
+def test_mac_takes_argument_and_creates_eui_object():
+    assert_equal(
+        utils.MAC('11:22:33:44:55:66'), netaddr.EUI('11:22:33:44:55:66')
+    )
+    assert_equal(
+        utils.MAC('12:34:56:78:9a:bc'), netaddr.EUI('12:34:56:78:9a:bc')
+    )
+
+
+def test_mac_has_valid_str_representation():
+    assert_equal(str(utils.MAC('11:22:33:44:55:66')), '11:22:33:44:55:66')
+    assert_equal(str(utils.MAC('12:34:56:78:9a:bc')), '12:34:56:78:9a:bc')
