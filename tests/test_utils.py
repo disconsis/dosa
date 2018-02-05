0c89556e5f4bed193e409b8cf6378c806172f666
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2018-02-05 16:55:00 +0530

    change to nose for testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,8 @@
-import unittest
+from nose.tools import assert_equal
 import utils
 import netaddr
 
 
-class MACTest(unittest.TestCase):
-
-    def test_mac_has_broadcast_addr(self):
-        self.assertEqual(str(utils.MAC.broadcast), 'ff:ff:ff:ff:ff:ff')
-        self.assertEqual(utils.MAC.broadcast, netaddr.EUI('ff:ff:ff:ff:ff:ff'))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_mac_has_broadcast_addr():
+    assert_equal(str(utils.MAC.broadcast), 'ff:ff:ff:ff:ff:ff')
+    assert_equal(utils.MAC.broadcast, netaddr.EUI('ff:ff:ff:ff:ff:ff'))
